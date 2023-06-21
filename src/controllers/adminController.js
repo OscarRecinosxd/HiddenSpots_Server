a286b2ad6b75e479e19e95d4114cade7e22cbd07
ivettepinto7 <00186319@uca.edu.sv>
2023-06-21 00:21:30 -0600

    Add: function to deactivate user

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 //CRUD
@@ -7,13 +7,11 @@ exports.getUser = (req, res) => {
   console.log(req.params.id);
   User.findByPk(req.params.id)
     .then((user) => {
-      if(user){
+      if (user) {
         return res.status(200).json(user);
+      } else {
+        return res.status(200).json({ result: "Theres no username" });
       }
-      else{
-        return res.status(200).json({result : "Theres no username"})
-      }
-      
     })
     .catch((err) => {
       res.status(400).json({ result: "Something went wrong" });
@@ -39,11 +37,14 @@ exports.createUser = async (req, res) => {
   const confirm_password = req.body.confirm_password;
   const role = req.body.role;
 
-  if(password !== confirm_password){
-    return res.status(400).json({ result: "Passwords don't match"})
+  if (password !== confirm_password) {
+    return res.status(400).json({ result: "Passwords don't match" });
   }
 
-  const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT));
+  const hashedPassword = await bcrypt.hash(
+    password,
+    parseInt(process.env.SALT)
+  );
 
   User.create({
     username: username.toLowerCase(),
@@ -69,7 +70,7 @@ exports.updateUser = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const rolId = req.body.rol;
-  let savedUser
+  let savedUser;
 
   User.findByPk(userId)
     .then((userToUpdate) => {
@@ -80,9 +81,9 @@ exports.updateUser = (req, res) => {
       //Falta verificar que no de error el save
       if (username && email && password && rolId) {
         userToUpdate.save();
-        savedUser = userToUpdate
+        savedUser = userToUpdate;
       } else {
-        savedUser = null
+        savedUser = null;
       }
     })
     .then(() => {
@@ -96,18 +97,20 @@ exports.updateUser = (req, res) => {
     });
 };
 
-//REMOVE
-exports.deleteUser = (req, res) => {
-  const userId = req.body.id;
+//DEACTIVATE
+exports.deleteUser = async (req, res) => {
+  const userId = req.params.id;
 
-  User.findByPk(userId)
-    .then((userToDelete) => {
-      userToDelete.destroy();
-    })
-    .then(() => {
-      res.status(200).json({ result: "User deleted!" });
-    })
-    .catch((err) => {
-      res.status(400).json({ result: "Something went wrong" });
-    });
+  const userToDeactivate = await User.findByPk(userId);
+
+  if (!userToDeactivate) {
+    return res.status(400).json({ result: "Something went wrong" });
+  }
+
+  const actualStatus = userToDeactivate.isActive;
+
+  userToDeactivate.isActive = !actualStatus;
+  await userToDeactivate.save();
+
+  return res.status(200).json({ result: "User updated successfully" });
 };
